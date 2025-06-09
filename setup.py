aab56b336f68774c8924a70a005676dc2bb5e82d
harsimran <hsdosanjh1234@gmail.com>
2025-06-09 12:48:01 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project that fetch the youtube comments and then do the sentiment analyis on top of that',
+    author='Harsimrandosanjh',
+    license='MIT',
+)
